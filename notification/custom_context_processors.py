5687143b995d859f1f08b3ef6d44472635690c3e
baron <baronchibuike@gmail.com>
2022-05-22 15:24:20 +0100

    initial commit

--- notification/custom_context_processors.py
+++ notification/custom_context_processors.py
@@ -2,7 +2,6 @@ from notification.models import BroadcastNotification
 
 def notifications(request):
     all_notifications = BroadcastNotification.objects.all()
-    print(all_notifications, '-------------')
     return {
         'notifications': all_notifications
     }
